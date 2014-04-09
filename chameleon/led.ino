3f36cc81e81a00d12bbe922ddf3f4adb4e44c101
Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>
2014-04-09 14:00:01 +0100

    LED show off

--- chameleon/led.ino
+++ chameleon/led.ino
@@ -1,10 +1,21 @@
-#define LED_MAX 70
+#define LED_RED_MAX   100
+#define LED_GREEN_MAX  75
+#define LED_BLUE_MAX  130
 
 void led_setup()
 {
     pinMode(RED_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(BLUE_PIN, OUTPUT);
+}
+
+void led_showColour(int color)
+{
+    int red   = pgm_read_byte_near(LED_VALS + color*3 + 0);
+    int green = pgm_read_byte_near(LED_VALS + color*3 + 1);
+    int blue  = pgm_read_byte_near(LED_VALS + color*3 + 2);
+
+    led_setLED(red,green,blue);
 }
 
 /* Set all the LEDs to a colour
@@ -15,13 +26,55 @@ void led_setup()
  *   a low absolute value, they look strange, so we cap
  *   them with the LED_MAX value.
  */
-void setLED(int red, int green, int blue)
+void led_setLED(int red, int green, int blue)
 {
-  int redOut   = map(red,   0,255, 0,LED_MAX);
-  int greenOut = map(green, 0,255, 0,LED_MAX);
-  int blueOut  = map(blue,  0,255, 0,LED_MAX);
+  int redOut   = map(red,   0,255, 0,LED_RED_MAX);
+  int greenOut = map(green, 0,255, 0,LED_GREEN_MAX);
+  int blueOut  = map(blue,  0,255, 0,LED_BLUE_MAX);
 
   analogWrite(RED_PIN,   255-redOut);
   analogWrite(GREEN_PIN, 255-greenOut);
   analogWrite(BLUE_PIN,  255-blueOut);
+}
+
+void led_showOff()
+{
+    led_setLED(0,0,0);
+    _red = 0;
+    _green = 0;
+    _blue = 0;
+
+    delay(100);
+
+    for (int i = 0; i < NUM_COLORS; i++)
+    {
+        int red   = pgm_read_byte_near(LED_VALS + i*3 + 0);
+        int green = pgm_read_byte_near(LED_VALS + i*3 + 1);
+        int blue  = pgm_read_byte_near(LED_VALS + i*3 + 2);
+
+        led_transition(red,green,blue);
+        delay(100);
+    }
+}
+
+int _red = 0;
+int _green = 0;
+int _blue = 0;
+
+void led_transition(int newRed, int newGreen, int newBlue)
+{
+    while (_red != newRed || green != newGreen || blue != newBlue)
+    {
+        if      (_red < newRed) _red++;
+        else if (_red > newRed) _red--;
+
+        if      (_green < newGreen) _green++;
+        else if (_green > newGreen) _green--;
+
+        if      (_blue < newBlue) _blue++;
+        else if (_blue > newBlue) _blue--;
+
+        led_setLED(_red,_green,_blue);
+        delay(1);
+    }
 }
