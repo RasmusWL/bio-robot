b7ff3ca4365b9b8bfb9ebb9edc1a9141838f59b3
Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>
2014-04-27 21:43:39 +0100

    encoder: correct wheel tick, dist travel function

--- chameleon/encoder.ino
+++ chameleon/encoder.ino
@@ -1,16 +1,35 @@
+#define WHEEL_CIRCUMFERENCE 14.137 // cm
+#define WHEEL_TICKS 155            // The number of 'ticks' for a full wheel cycle
+
+// 155 seems to work! for only rising ;)
+
+// 12 encoder ticks per motor reveloution
+// 51.45 : 1 gear ratio
+
 void encoder_setup()
 {
     pinMode(ENCODER_LEFT_A, INPUT);
-    //pinMode(ENCODER_LEFT_B, INPUT);
 
     pinMode(ENCODER_RIGHT_A, INPUT);
-    //pinMode(ENCODER_RIGHT_B, INPUT);
 
+    encoder_reset();
+
+    attachInterrupt(0, leftEncoderEvent, RISING);
+    attachInterrupt(1, rightEncoderEvent, RISING);
+}
+
+void encoder_reset()
+{
     leftCount = 0;
     rightCount = 0;
+}
 
-    attachInterrupt(0, leftEncoderEvent, CHANGE);
-    attachInterrupt(1, rightEncoderEvent, CHANGE);
+float encoder_distance_traveled()
+{
+    // TODO : under the assumption that both motors and encoders work the same
+
+    float avg = (leftCount + rightCount) / 2;
+    return (avg * WHEEL_CIRCUMFERENCE) / WHEEL_TICKS;
 }
 
 void leftEncoderEvent()
