dfb1889cdd0775b71d86fa82aee5391cf9829323
Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>
2014-05-04 16:53:39 +0100

    colordata: small adjustments

--- chameleon/colordata.h
+++ chameleon/colordata.h
@@ -5,7 +5,7 @@ PROGMEM const unsigned char DETECTION_LOW_VALS[NUM_COLORS * 4] =
   240, 110,  60, 110, // RED
   130, 170,  70, 105, // GREEN
   125, 190, 190, 130, // BLUE
-  240, 240, 220, 210, // WHITE
+  240, 240, 210, 190, // WHITE
   217, 200,  80, 162, // ORANGE
   240, 240, 110, 190  // YELLOW
 };
@@ -17,7 +17,7 @@ PROGMEM const unsigned char DETECTION_HIGH_VALS[NUM_COLORS * 4] =
   175, 255, 240, 170, // BLUE
   255, 255, 255, 255, // WHITE
   255, 255, 110, 187, // ORANGE
-  255, 255, 140, 230  // YELLOW
+  255, 255, 145, 250  // YELLOW
 };
 
 PROGMEM const unsigned char LED_VALS[NUM_COLORS * 3] =
