74496ff706441c86c8fcf1ecb9fdbb0850073795
Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>
2014-05-06 12:05:46 +0100

    avoidance: completely rewritten

--- chameleon/avoidance.ino
+++ chameleon/avoidance.ino
@@ -1,73 +1,29 @@
-void avoid_avoid(info_t* info, action_t* action)
+bool avoidance_needToStop(prox_t prox)
 {
-    // TODO : has the front sensor been adjusted? (as it is positionsed more forwards)
+    return ( prox.middle < DIST_MIDDLE_STOP || prox.right < DIST_SIDE_STOP || prox.left < DIST_SIDE_STOP );
+}
 
-    int proxy0CM = info->proxy0CM;
-    int proxy1CM = info->proxy1CM;
-    int proxy2CM = info->proxy2CM;
+bool avoidance_shouldNotGoThisWay(prox_t prox)
+{
+    return ( prox.middle < DIST_MIDDLE_MEDIUM || prox.right < DIST_MIDDLE_MEDIUM || prox.left < DIST_MIDDLE_MEDIUM );
+}
 
-    if ( proxy0CM > 30 && proxy1CM > 30 && proxy2CM > 30 )
+bool avoidance_shouldNotGoThisWayMiddleOnly(prox_t prox)
+{
+    return ( prox.middle < DIST_MIDDLE_MEDIUM || prox.right < DIST_SIDE_STOP || prox.left < DIST_SIDE_STOP );
+}
+
+int avoidance_turnPref(prox_t prox)
+{
+    if ( prox.right > DIST_SIDE_MEDIUM )
     {
-        return;
+        return TURN_WAY_RIGHT;
+    }
+    else if ( prox.left > DIST_SIDE_MEDIUM )
+    {
+        return TURN_WAY_LEFT;
     }
 
-    action->type = ACTION_TURN;
-
-    if ( proxy0CM < proxy1CM-2 && proxy1CM < proxy2CM-2 )
-    {
-        /* Should turn Right
-         *
-         *        \
-         *  0 =>   \
-         *  1 =>    \
-         *  2 =>     \
-         *            \
-         */
-
-        action->param = SMALLEST_TURN;
-    }
-    else if (proxy2CM < proxy1CM-2 && proxy1CM < proxy0CM-2)
-    {
-        /* Should turn Left
-         *
-         *            /
-         *  0 =>     /
-         *  1 =>    /
-         *  2 =>   /
-         *        /
-         */
-
-        action->param = -SMALLEST_TURN;
-    }
-    else
-    {
-
-        /* Something comepletely else, maybe something like (or opposite)
-         *
-         *            /
-         *  0 =>     /
-         *  1 =>    (
-         *  2 =>     \
-         *            \
-         */
-
-        /* Or if sensors are accurate
-         *
-         *          |
-         *  0 =>    |
-         *  1 =>    |
-         *  2 =>    |
-         *          |
-         */
-
-
-        // TODO : take the history into consideration
-        if (action->turnPreference == 0)
-        {
-            if (random(1) == 0) action->turnPreference = -1;
-            else                action->turnPreference =  1;
-        }
-
-        action->param = action->turnPreference * SMALLEST_TURN;
-    }
+    if (random(0, 2) == 0) { return TURN_WAY_LEFT; }
+    else                   { return TURN_WAY_RIGHT; }
 }
